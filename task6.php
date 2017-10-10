ed817c3272c9d5f563d95e6fe4b216cc21b9fb81
Margarita Samsonova <pitasam@yandex.ru>
2017-10-10 15:49:38 +0300

    Домашняя работа №1

---
+++ task6.php
@@ -0,0 +1,26 @@
+<?php
+/**
+Задание #6
+
+Создайте массив $bmw с ячейками:
+model
+speed
+doors
+year
+Заполните ячейки значениями соответсвенно: “X5”, 120, 5, “2015”
+Создайте   массивы   $toyota   и   $opel   аналогичные   массиву   $bmw   (заполните  данными)
+Объедините три массива в один многомерный массив
+Выведите значения всех трех массивов в виде:
+CAR name
+name ­ model ­speed ­ doors ­ year
+Например:
+CAR bmw
+X5 ­120 ­ 5 ­ 2015
+ */
+$bmw = ["model" => "X5","speed" => 120, "doors" => 5, "year" => "2015"];
+$toyota = ["model" => "RAV4", "speed" => 170, "doors" => 5 ,"year" => "2007"];
+$opel = ["model" => "Speedster", "speed" => 220, "doors" => 2 ,"year" => "2003"];
+
+$car = ["bmw" => $bmw, "toyota" => $toyota, "opel" => $opel];
+print_r($car["bmw"]);
+
